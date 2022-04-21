03b17da8a359f944411a33dabd3bf41565f5cfd0
Teo González Calzada <teo.gonzalez.c@hotmail.com>
2022-04-21 18:25:45 +0000

    fix: Event solution for the event can be null

---
+++ databases/migrations/2022_04_20_222630_events-fix.py
@@ -0,0 +1,19 @@
+"""Events-fix Migration."""
+
+from masoniteorm.migrations import Migration
+
+
+class EventsFix(Migration):
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.table("station_events") as table:
+          # Makes the event_solution_id nullable, because it can be null if there is no a solution yet
+          table.integer("event_solution_id").nullable()
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        pass
