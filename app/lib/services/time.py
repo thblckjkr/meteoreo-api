e89e399beeaedd8ba5b835158584cf042f6f35ee
Teo González Calzada <teo.gonzalez.c@hotmail.com>
2022-04-28 16:19:48 +0000

    fix: Better debugging of time and better error handling

--- app/lib/services/time.py
+++ app/lib/services/time.py
@@ -3,10 +3,10 @@ import time
 # Checks the time (station time is important, to preserve data consistency
 
 # Get's and compares the datetime against the current time in bash
-command = 'TARGET=$(date -d {0} +%s); CURRENT=$(date +%s); MINUTES=$(( ($TARGET - $CURRENT) / 60 ));'.format(int(time.time()))
+command = 'CURRENT=$(date +%s); MINUTES=$(( ({0} - $CURRENT) / 60 ));'.format(int(time.time()))
 
 # Checks if the obtained minutes is greater than 15 minutes, prints true if it is.
-command += 'if [ $MINUTES -gt 15 ]; then echo "true $MINUTES"; else echo "false"; fi'
+command += 'if (( $MINUTES > 15 || $MINUTES < -15 )); then echo "true $MINUTES"; else echo "false $MINUTES"; fi'
 
 service = {
     "command": command,
@@ -15,6 +15,14 @@ service = {
 
     "actions": {
         "bad_time": {
+          "description": "La fecha es incorrecta",
+          "solution": "Verifique la fecha y hora del sistema",
+          "response_stdout": "true",
+          "response_stderr": None,
+          "action": "Ponga la estación a la hora correcta con el comando date -s ",
+        },
+
+        "time_error": {
             "description": "Por alguna extraña razón el comando date '+%\s' dió error",
             "solution": "Por favor, revisa el comando date en la estación",
             "response_stderr": "",
