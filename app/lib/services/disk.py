039dd14307877e9a6c87b21dcc2f83cb702520ca
Teo González Calzada <teo.gonzalez.c@hotmail.com>
2022-05-20 02:31:46 -0600

    fix: disk service

--- app/lib/services/disk.py
+++ app/lib/services/disk.py
@@ -1,5 +1,5 @@
 service = {
-    "command": "PERCENT=$(df /mnt/usb --output=pcent | tail -n 1 | tr -d '%')" + \
+    "command": "PERCENT=$(df /mnt/usb --output=pcent | tail -n 1 | tr -d '%'); " + \
       "if (( $PERCENT > 80 )); then echo \"true $PERCENT\"; else echo \"false $PERCENT\"; fi",
     "stdout": "false",
     "stderr": None,
